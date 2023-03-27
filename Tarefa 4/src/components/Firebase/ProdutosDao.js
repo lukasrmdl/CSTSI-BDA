3a99331b02e87661c2600d4fb0e479c3510718c9
Lukas Raphael Mendes Duarte Lourençon <lukas.rmdl@gmail.com>
2023-03-26 22:28:46 -0300

    Fazendo tarefa 4

--- Tarefa 4/src/components/Firebase/ProdutosDao.js
+++ Tarefa 4/src/components/Firebase/ProdutosDao.js
@@ -3,15 +3,7 @@ import {
     query,
     orderByChild,
     onChildAdded,
-    off,
-    endAt,
-    endBefore,
-    equalTo,
-    startAt,
-    startAfter,
-    onValue,
-    limitToFirst,
-    limitToLast
+    startAt
 } from "firebase/database"
 
 function getOrderByChild(order,db,callback){
@@ -23,31 +15,24 @@ function getOrderByChild(order,db,callback){
 
 function getFilterByChild(filter,value, db,callback){
     console.log(filter)
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB,orderByChild(filter), startAt(value))
+    onChildAdded(consulta,callback)
 }
 
-function getMostExpensive(db,setValue,list){
-    // implement aqui
-    /**
-     *     Nesta função é necessário implementar o callback,
-     * pois será necessário ordenar os resultados no cliente
-     * pelos produtos mais caros (reverso). É necessário chamar 
-     * a função setValue() e o array list passados como parametro.
-     * Para repassar os resultados do client React utiliza-se a função
-     * setValue() com os parametros [...list], ou seja, setValue([...list])
-     * onde em list deverá estar o array de produtos ordenados pelo preco
-     * mais caro. Lembrando que list é um array, use os métodos para trabalhar
-     * com arrays em JavaScript! Dica: usem reverse() ou unshift().
-     * 
-     *  */ 
-
-
+function getMostExpensive(db,setValue,list,callback){
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB, orderByChild('preco'));
+    onChildAdded(consulta,callback)
 }
 
 function getMostCheap(db,callback){
-    //implemente aqui
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB, orderByChild('preco').limitToFirst(10));
+    onChildAdded(consulta,callback)
 }
 
-function getPriceRange(value, db,callback){//0--->limit
+function getPriceRange(value, db,callback){
     //implemente aqui
 }
 

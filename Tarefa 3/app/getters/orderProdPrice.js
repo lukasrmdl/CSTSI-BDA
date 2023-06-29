b92729f0492e0150f58e2fa6d4dc6c3da6a604a1
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-06-28 23:37:34 -0300

    fix: uso de orderByChild para ordenar. orderByChild ordena por um campo específico, neste caso por "preco"

--- Tarefa 3/app/getters/orderProdPrice.js
+++ Tarefa 3/app/getters/orderProdPrice.js
@@ -1,11 +1,11 @@
-import { ref, query, orderByValue, onChildAdded, child } from "firebase/database";
+import { ref, query, orderByChild, onChildAdded, child } from "firebase/database";
 import database from "../../database/database.js";
 const dbRef = ref(database, "produtos");
 
-const consulta = query(dbRef, orderByValue());
+const consulta = query(dbRef, orderByChild('preco'));
 onChildAdded(consulta, (dados) => {
   let chave = dados.key;
   let valor = dados.val();
-  console.log(`${chave} : ${valor}`);
+  console.log(`${chave} : ${JSON.stringify(valor, null, 4)}`);
 });
-setTimeout(() => process.exit(0), 5000);
+setTimeout(() => process.exit(0), 3000);

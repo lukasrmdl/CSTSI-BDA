541501045a02c49be7c32b118a2e4f322d94a51a
Lukas Raphael Mendes Duarte Lourençon <lukas.rmdl@gmail.com>
2023-07-12 20:15:10 -0300

    finalizando tarefa 9

---
+++ Tarefa 8 - 9/tarefa-9/exercicio-a/exercicio-9-a.js
@@ -0,0 +1,42 @@
+import { MongoClient } from 'mongodb'
+
+const myDB = {
+    server: '127.0.0.1',
+    port: 27017,
+}
+
+const uri = `mongodb://${myDB.server}:${myDB.port}`;
+const client = new MongoClient(uri);
+
+try {
+    await client.connect()
+    
+    if (client.db('banco-bda').command({ "ping": 1 }))
+        console.log("Conectado!");
+    else 
+        throw Error("Erro ao conectar ao banco !!")
+
+    const termo = "GAMER";
+
+    await client.db('banco-bda').collection('produtos').createIndex({ descricao: 'text' });
+
+    const filtro = {//filter
+        $text: {
+            $search: termo
+        }
+    }
+
+    const opcoes = {//options
+        projection: {_id: 0, qtd_estoque: 0, id_prod: 0, nome: 0, preco: 0, importado: 0, desconto: 0} //projeção
+    }
+
+    const resultados = await client.db('banco-bda').collection('produtos').find(filtro, opcoes).toArray()
+    console.table(resultados);
+}
+catch (error) {
+    console.log(error);
+} 
+finally {
+    await client.close();
+    process.exit(0);
+}
